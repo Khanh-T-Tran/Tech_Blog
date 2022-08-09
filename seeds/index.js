647ba233ba464badbcb835bce84ef8bdeeb49089
Khanh-T-Tran <khanhtuantran01@gmail.com>
2022-08-08 21:35:43 -0700

    fixed node seeds issue

--- seeds/index.js
+++ seeds/index.js
@@ -4,7 +4,9 @@ const sequelize = require('../config/connection');
 
 
 const seeder = async () => {
+    // console.log('hi');
     // wipes out the user table
+    // force: true adds a DROP TABLE IF EXISTS before trying to create the table - if you force, existing tables will be overwritten.
     await sequelize.sync({ force: true });
     await User.bulkCreate(users);
     // stop the current node process that hangging and exit node seeds with 0 mean no error happened/ 1 mean an error happened
@@ -12,5 +14,6 @@ const seeder = async () => {
 };
 
 (async () => {
+    // console.log('hi seeds');
     await seeder();
-})()
+})();
--- seeds/users.js
+++ seeds/users.js
@@ -1,14 +1,17 @@
 module.exports = [
     {
+        id: 1,
         username: 'khanh tran',
         password: 'password'
     },
     {
+        id: 2,
         username: 'khanh tran 2',
         password: 'password'
 
     },
     {
+        id: 3,
         username: 'khanh tran 3',
         password: 'password'
 
