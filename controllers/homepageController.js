4ede99dae609726560e03a5ff9f638746c9995a3
Khanh-T-Tran <khanhtuantran01@gmail.com>
2022-08-09 15:49:50 -0700

    created landing_page, modified homepageController, model index, seeds user and user handlebars

--- controllers/homepageController.js
+++ controllers/homepageController.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
 const { User } = require('./../models');
 
-// router.get('/', (req, res) => {
-//     res.render('', { signup });
-// });
+router.get('/', (req, res) => res.render('landing_page'));
 
 // HTML ROUTES + API POST,DELETE,PUT/PATCH routes
 router.get('/users', async (req, res) => {
@@ -11,14 +9,14 @@ router.get('/users', async (req, res) => {
         // Find all the user in the database
         const dbusersData = await User.findAll();
         console.log(dbusersData);
-        // for each user in dbUser we going to return dbUser, get the data in plain json
+        // for each user in dbUser we going to return dbUser, then get the data insql objects and turn to plain json user objects
         const users = dbusersData.map(dbUser => dbUser.get({ plain: true }));
         console.log(users);
         // the array of plain json objects will be feed into the users template
         res.render('users', { users });
 
     } catch (error) {
-        console.log('Err L16 homepageController', error);
+        console.log('Err L21 homepageController', error);
         res.status(500).json(error);
     }
 });
